4563723ccbb64e518ae000991e9f1961c5c16d7f
sristi ghimire <ghimiresristi9@gmail.com>
2022-07-01 09:34:53 +0545

    msg

---
+++ example.js
@@ -0,0 +1,67 @@
+/*function helloworld{
+    const name = 'Hello';
+    console.log('Type of name ${typeof name}');
+    }
+    function check(a,b){
+    if(a%2===0){
+        console.log('EVEN');
+    }
+    else{
+        console.log('ODD');
+    }
+
+    /*for(let i=0;i<b;i++){
+        console.log('\n B iteration ${i}');
+    }*/
+
+/*check(20,65);
+
+function spare(a,b,c,...f)
+{
+    if(d===e)
+    {
+    console.log('TRUE');
+    }
+    else{
+        console.log('FALSE');
+    }
+    console.log(f)
+}
+spare(2,5,7,23,9,3); */
+
+
+// Fibonacci series
+/*function fibonacci()
+{
+    const number=15;
+    let a=0,b=1;
+    let c;
+    console.log('Fibonacci Series');
+    for(let i=1;i<=number;i++){
+        console.log(a);
+        c=a+b;
+        a=b;
+        b=c;
+    }
+}
+fibonacci()
+*/ 
+
+
+// recursive
+/*function recursive(n){
+    if(n<2){
+        return 1;
+    }
+    else{
+        return(recursive(n-1)+recursive(n-2));
+    }
+
+}
+for(let i=0;i<=20;i++){
+    console.log(recursive(i));
+}
+*/
+
+
+
